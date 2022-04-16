e6da086b63768284c5d2c009e5be5e21da24122c
daroil <gendel2403@gmail.com>
2022-04-16 16:31:08 +0200

    a lot of work

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,71 @@
+'use strict';
 let img = document.querySelector('#img');
-let notification = document.querySelector("#notification")
-img.addEventListener("click",hide);
-function hide() {
-	
+let notification = document.querySelector("#notification");
+let back = document.querySelector("#back");
+
+
+let circle = document.createElement('div');
+
+let count = 0;
+
+img.addEventListener("click",show);
+back.addEventListener("click",hide);
+request.addEventListener("click",sentReq);
+
+
+
+function show() 
+{
+    notification.classList.remove("hidden");
+    count = 0;
+    if (document.getElementById("circle")) 
+    {
+        circle.remove();
+    }
+}
+
+function hide() 
+{
+    notification.classList.add("hidden");
+}
+
+function sentReq() 
+{
+    if (document.getElementById("circle")) 
+    {
+        circle.remove();
+    }
+    count +=1;
+    const val = document.querySelector('#form').value;
+    let head = document.querySelector("header");
+    circle.setAttribute('id', 'circle') 
+    circle.innerHTML = count;
+    document.querySelector('#form').value = "";
+    console.log(val);
+    circle.className =  'circle';
+    head.append(circle);
+    requestProccessing(val);
+}
+
+function requestProccessing(val) 
+{
+    let text = document.createElement('div');
+    let wrap = document.createElement('div');   
+    let input = document.createElement('div')
+    if (val) 
+    {
+        text.className = 'success';
+        text.innerHTML = 'Your request was succesfully processed!'
+    }
+    else 
+    {
+        text.className = 'alert';
+        text.innerHTML = 'Your request was declined, sorry'
+    }
+    input.innerHTML = val;
+    wrap.className = 'wrapper';
+    notification.append(wrap);
+    text.append(input);
+    wrap.append(text);
+    
 }
